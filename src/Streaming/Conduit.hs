020ee73c0249b30dc428c31f7c30bfdcb75c7549
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-06-08 10:12:37 +1000

    Use the Conduit type alias

--- src/Streaming/Conduit.hs
+++ src/Streaming/Conduit.hs
@@ -14,8 +14,8 @@ module Streaming.Conduit where
 
 import           Control.Monad             (join, void)
 import           Control.Monad.Trans.Class (lift)
-import           Data.Conduit              (ConduitM, Producer, Source, await,
-                                            runConduit, yield, (.|))
+import           Data.Conduit              (Conduit, ConduitM, Producer, Source,
+                                            await, runConduit, yield, (.|))
 import qualified Data.Conduit.List         as CL
 import           Streaming                 (Of, Stream, hoist)
 import qualified Streaming.Prelude         as S
@@ -51,16 +51,16 @@ toStream cnd = runConduit (cnd' .| mkStream)
 
     cnd' = hoist lift cnd
 
--- | Treat a 'ConduitM' as a function between 'Stream's.  Subject to
+-- | Treat a 'Conduit' as a function between 'Stream's.  Subject to
 --   fusion.
-asStream :: (Monad m) => ConduitM i o m () -> Stream (Of i) m () -> Stream (Of o) m ()
+asStream :: (Monad m) => Conduit i m o -> Stream (Of i) m () -> Stream (Of o) m ()
 asStream cnd stream = toStream (src .| cnd)
   where
     src = fromStreamProducer stream
 
--- | Treat a function between 'Stream's as a 'ConduitM'.  May be
+-- | Treat a function between 'Stream's as a 'Conduit'.  May be
 --   subject to fusion.
-asConduit :: (Monad m) => (Stream (Of i) m () -> Stream (Of o) m r) -> ConduitM i o m ()
+asConduit :: (Monad m) => (Stream (Of i) m () -> Stream (Of o) m r) -> Conduit i m o
 asConduit f = join . fmap (fromStreamProducer . f) $ go
   where
     -- Probably not the best way to go about it,
